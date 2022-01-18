96adde6add7e22b9811dc713ab041638778857f8
Boussett4u <nael.boussetta3@etu.univ-lorraine.fr>
2022-01-18 19:59:04 +0100

    ajout d'un script d'automatisation d'installation

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y python3-pip
+pip3 install package_name
+sudo apt install -y build-essential libssl-dev libffi-dev python-dev
+sudo apt install -y python3-venv
+. env/bin/activate
+pip install flask
+pip install flask-restplus
