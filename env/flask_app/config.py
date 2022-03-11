08db970f754242f433655e5b7c4ce8100dd53cbe
Boussett4u <nael.boussetta3@etu.univ-lorraine.fr>
2022-03-11 18:38:52 +0100

    ajout fichier config

---
+++ env/flask_app/config.py
@@ -0,0 +1,24 @@
+# Cette chaîne nous permet de nous connecter à la bdd
+# bdd_uri = "postgresql://postgres:nael2001@127.0.0.1:5432/test"
+bdd_uri = { 'pguser':'postgres',
+'pgpasswd':'nael2001',
+'pghost':'localhost',
+'pgport': 5432,
+'pgdb':'test'}
+
+mdp_admin = "nael2001"
+
+mdp_captcha = "nael2001"
+
+mdp_adminnohash = "nael2001"
+
+
+site_key = '6LfJt0IeAAAAAJzXffa3y4XckjEa3IHRYiQtQ54U'
+secret_key = '6LfJt0IeAAAAAJgNyIow5R81ACZPnk6ZKTC5-FHQ'
+# site_key = '6LfAIpIeAAAAAIwhkZcm3lpxLWGEwFVDyD--P0Jf'
+# secret_key = '6LfAIpIeAAAAAI2mn-My9_dXjG9qZ1E3loImHKms'
+
+
+ACCEPTED=1
+REFUSED=2
+UNDECIDED=3 
