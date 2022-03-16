5d5c285508e7214a7ff3e27d6be73b04d55d57ef
william <wgrd.wgerardfr@gmail.com>
2022-03-16 20:18:56 +0100

    correctif

--- docker/postfix/scripts/classes.py
+++ docker/postfix/scripts/classes.py
@@ -9,14 +9,14 @@ from flask_sqlalchemy import SQLAlchemy
 #Importation des variables du docker-compose
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
-db_ip = os.getenv('DB_IP')
+db_host = os.getenv('DB_HOST')
 db_name = os.getenv('DB_NAME')
 db_port = os.getenv('DB_PORT')
 
 #Configuration pour utiliser flask et SQLAlchemy               
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = ("postgresql://"+db_user+":"+db_password+"@"+db_ip+":"+db_port+"/"+db_name)
+app.config['SQLALCHEMY_DATABASE_URI'] = ("postgresql://"+db_user+":"+db_password+"@"+db_host+":"+db_port+"/"+db_name)
 db = SQLAlchemy(app)
 
 #Création des classes correspondant aux tables de la base de données (utilisé aussi pour la création de la base en phase de test)
